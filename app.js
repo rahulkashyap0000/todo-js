7597c631bfcdf3a8414870b6b3dc39f75aee3e48
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-06-01 13:38:35 +0530

    Filter task done

--- app.js
+++ app.js
@@ -68,3 +68,15 @@ function clearTasks() {
     // Clear Tasks from Local Storage
     clearTasksFromLocalStorage();
 }
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll(".collection-item").forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = "block";
+        } else {
+            task.style.display = "none";
+        }
+    });
+}
